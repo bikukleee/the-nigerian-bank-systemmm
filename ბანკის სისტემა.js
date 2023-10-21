52fbfc868a4ca79d8665cc7d6f087ade2f76c6c9
bikukleee <dontworryaoutme@gmail.com>
2023-10-21 18:21:44 +0400

    nbon

--- ბანკის სისტემა.js
+++ ბანკის სისტემა.js
@@ -1,35 +1,27 @@
 document.getElementById("deposit").addEventListener("click", deposit);
 document.getElementById("withdraw").addEventListener("click", withdraw);
 document.getElementById("balanceCheck").addEventListener("click", checkBalance);
-
 document.getElementById("exit").addEventListener("click", exit);
 
-
-let balance = 0;
+let balance = parseFloat(localStorage.getItem('balance')) || 0;
 
 function deposit() {
-    const amount = parseFloat(prompt("enter the deposit amount:"));
-    if (isNaN(amount) || amount <= 0){
-        alert("please enter valid amount")
+    const amount = Number(prompt("Enter the deposit amount:"));
+    if (amount <= 0 || isNaN(amount)) {
+        alert("Please enter a valid amount.");
     } else {
-
         balance += amount;
         updateBalance();
     }
-
-
 }
 
 function withdraw() {
-    const amount = parseFloat(prompt("Enter the withdrawal amount:"));
-    if (isNaN(amount) || amount <= 0) {
-        alert("Invalid amount. Please enter a valid amount.");
-
-
+    const amount = Number(prompt("Enter the withdrawal amount:"));
+    if (amount <= 0 || isNaN(amount)) {
+        alert("Please enter a valid amount.");
     } else if (amount > balance) {
         alert("Insufficient balance.");
     } else {
-
         balance -= amount;
         updateBalance();
     }
@@ -39,16 +31,16 @@ function checkBalance() {
     alert(`Current balance: $${balance}`);
 }
 
-
 function exit() {
+    // Save the balance to Local Storage
+    localStorage.setItem('balance', balance);
+
     alert("Thank you for using our banking system. Have a great day!");
     window.location.href = "რეგისტრაციის გვერდი.html";
 }
 
 function updateBalance() {
-    document.getElementById("balance").textContent = `balance: $${balance}`;
+    document.getElementById("balance").textContent = `Balance: $${balance}`;
 }
-
-
 
 updateBalance();
--- რეგისტრაციის.js
+++ რეგისტრაციის.js
@@ -3,14 +3,19 @@ document.getElementById("signup").addEventListener("submit", function (event) {
     const name = document.getElementById("namesb").value;
     const password = document.getElementById("password").value;
 
-    if (password === ""){
-        alert("please enter valid password")
+    if (password === "") {
+        alert("Please enter a valid password");
     } else {
-        alert(`thank you for signing up ${name} your new account is created and ready to use`);
-        window.location.href = "ბანკი.html"
+        // Check if the user's credentials already exist in Local Storage
+        if (localStorage.getItem('namesb') === name && localStorage.getItem('password') === password) {
+            alert("Welcome back, " + name);
+            window.location.href = "ბანკი.html";
+        } else {
+            localStorage.setItem('namesb', name);
+            localStorage.setItem('password', password);
+            localStorage.setItem('balance', 0); // Set initial balance to 0 for each new user
+            alert(`Thank you for signing up, ${name}. Your new account is created and ready to use.`);
+            window.location.href = "ბანკი.html";
+        }
     }
-
-
-
-
-})
+});
